815ad7da9a740d6fb27e6925a4e6373ce9fab822
Freight Trust <4074885-freighttrust@users.noreply.gitlab.com>
2020-03-21 16:31:07 -0700

    Update utils.ts
    adding chaind: 211 for freight-trust

--- src/superblocks/utils.ts
+++ src/superblocks/utils.ts
@@ -14,9 +14,9 @@
 // You should have received a copy of the GNU General Public License
 // along with Superblocks.  If not, see <http://www.gnu.org/licenses/>.
 
-import { injectable } from 'inversify';
-import { ISuperblocksUtils } from '../ioc/interfaces';
-import { ICustomMetadataModel, IMetadataModel } from './models';
+import {injectable} from 'inversify';
+import {ISuperblocksUtils} from '../ioc/interfaces';
+import {ICustomMetadataModel, IMetadataModel} from './models';
 import fs from 'fs';
 import path from 'path';
 
@@ -26,7 +26,7 @@ export class SuperblocksUtils implements ISuperblocksUtils {
         return process.env.API_URL || `https://api.superblocks.com/v1`;
     }
 
-    networkIdToName(networkId: string): string {
+    networkIdToName(networkId : string): string {
         switch (networkId) {
             case '1':
                 return 'Mainnet';
@@ -38,38 +38,45 @@ export class SuperblocksUtils implements ISuperblocksUtils {
                 return 'Görli';
             case '42':
                 return 'Kovan';
+            case '211':
+                return 'Freight-Trust'
             default:
                 return networkId;
-    }
+        }
     }
 
-    createDefaultMetadata(metadata: ICustomMetadataModel, ciJobId: string): IMetadataModel {
-        const { jobId, jobURL, description, hash, branch, branchUrl, commitUrl } = metadata || {};
-        const { env } = process;
+    createDefaultMetadata(metadata : ICustomMetadataModel, ciJobId : string): IMetadataModel {
+        const {
+            jobId,
+            jobURL,
+            description,
+            hash,
+            branch,
+            branchUrl,
+            commitUrl
+        } = metadata || {};
+        const {env} = process;
 
         // env variables from metadata object, Superblocks, Circle CI, Travis CI, Gitlab and Jenkins respectively
         return {
-            jobId : jobId || env.CIRCLE_WORKFLOW_ID || env.TRAVIS_JOB_ID || env.CI_JOB_ID || env.BUILD_ID,
-            jobURL : jobURL || env.CIRCLE_BUILD_URL || env.CI_JOB_URL || env.TRAVIS_JOB_WEB_URL || env.BUILD_URL,
-            description : description || env.SUPER_COMMIT_DESCRIPTION || env.CI_COMMIT_MESSAGE || env.TRAVIS_COMMIT_MESSAGE,
-            hash : hash || env.SUPER_COMMIT_SHA1 || env.CIRCLE_SHA1 || env.TRAVIS_COMMIT || env.CI_COMMIT_SHA,
-            branch : branch || env.SUPER_COMMIT_BRANCH || env.CIRCLE_BRANCH || env.TRAVIS_PULL_REQUEST_BRANCH || env.CI_COMMIT_REF_NAME || env.COMMIT_BRANCH,
-            branchUrl : branchUrl || env.SUPER_COMMIT_BRANCH_URL || env.CIRCLE_REPOSITORY_URL || env.CI_REPOSITORY_URL,
-            commitUrl : commitUrl || env.SUPER_COMMIT_URL,
+            jobId: jobId || env.CIRCLE_WORKFLOW_ID || env.TRAVIS_JOB_ID || env.CI_JOB_ID || env.BUILD_ID,
+            jobURL: jobURL || env.CIRCLE_BUILD_URL || env.CI_JOB_URL || env.TRAVIS_JOB_WEB_URL || env.BUILD_URL,
+            description: description || env.SUPER_COMMIT_DESCRIPTION || env.CI_COMMIT_MESSAGE || env.TRAVIS_COMMIT_MESSAGE,
+            hash: hash || env.SUPER_COMMIT_SHA1 || env.CIRCLE_SHA1 || env.TRAVIS_COMMIT || env.CI_COMMIT_SHA,
+            branch: branch || env.SUPER_COMMIT_BRANCH || env.CIRCLE_BRANCH || env.TRAVIS_PULL_REQUEST_BRANCH || env.CI_COMMIT_REF_NAME || env.COMMIT_BRANCH,
+            branchUrl: branchUrl || env.SUPER_COMMIT_BRANCH_URL || env.CIRCLE_REPOSITORY_URL || env.CI_REPOSITORY_URL,
+            commitUrl: commitUrl || env.SUPER_COMMIT_URL,
             ciJobId,
-            buildConfigId : env.SUPER_BUILD_CONFIG_ID,
+            buildConfigId: env.SUPER_BUILD_CONFIG_ID,
             superblocks: env.SUPER_CI || 'false'
         };
     }
 
-    saveDeploymentInfo(deploymentId: string, token: string) {
+    saveDeploymentInfo(deploymentId : string, token : string) {
         const folder = '.superblocks';
         if (!fs.existsSync(folder)) {
             fs.mkdirSync(folder);
         }
-        fs.writeFileSync(path.join(folder, 'deployment.json'), JSON.stringify({
-            deploymentId,
-            token
-        }));
+        fs.writeFileSync(path.join(folder, 'deployment.json'), JSON.stringify({deploymentId, token}));
     }
 }
